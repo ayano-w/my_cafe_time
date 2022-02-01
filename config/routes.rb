3dbe7b1ceeface77c6bf842fe7db58794a110965
ayano-w <ayasaku.t5@gmail.com>
2022-02-01 22:37:44 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  
-  
+
+  resources :users, only: [:update]
+  get 'users/mypage' => 'users#show', as: 'mypage'
+
+  resources :caves
+  resources :favorites, only: [:create, :destroy]
+  resources :categories, only: [:create, :destroy]
+
 end
