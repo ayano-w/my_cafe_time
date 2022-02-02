2a513bef610367e789e7e35e83ecd7f11484c8db
ayano-w <ayasaku.t5@gmail.com>
2022-02-02 18:29:36 +0000

    add cafe show

--- app/controllers/caves_controller.rb
+++ app/controllers/caves_controller.rb
@@ -1,4 +1,6 @@
 class CavesController < ApplicationController
+  before_action :set_cafe, except:[:new, :create, :index]
+
   def new
     @cafe = Cafe.new
   end
@@ -17,22 +19,31 @@ class CavesController < ApplicationController
   end
 
   def show
-    @cafe = Cafe.find(params[:id])
   end
 
   def edit
-    @cafe = Cafe.find(params[:id])
   end
 
   def update
+    if @cafe.update(cafe_params)
+      redirect_to cafe_path(@cafe.id), notice: '更新しました'
+    else
+      redirect_to edit_cafe_path(@cafe.id)
+    end
   end
 
   def destroy
+    @cafe.destroy
+    redirect_to caves_path, notice: '投稿を削除しました'
   end
 
   private
 
   def cafe_params
     params.require(:cafe).permit(:user_id, :category_id, :tea, :teacake, :tea_image, :comment)
+  end
+
+  def set_cafe
+    @cafe = Cafe.find(params[:id])
   end
 end
--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -5,7 +5,7 @@ class Cafe < ApplicationRecord
   validates :tea, presence: true, length: { maximum: 15 }
   validates :teacake, length: { maximum: 15 }
   validates :tea_image, presence: true
-  validates :comment, length: { maximum: 20 }
+  validates :comment, length: { maximum: 100 }
 
   # def favorited_by?(user)
   #   favorites.where(user_id: user.id).exists?
