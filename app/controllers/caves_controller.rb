3e0d573ce1cf76d02b074000161054f88ea5beca
ayano-w <ayasaku.t5@gmail.com>
2022-02-02 00:35:33 +0000

    add cafe controller

--- app/controllers/caves_controller.rb
+++ app/controllers/caves_controller.rb
@@ -1,7 +1,35 @@
 class CavesController < ApplicationController
   def new
+    @cafe = Cafe.new
+  end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      redirect_to caves_path
+    else
+      redirect_to new_cafe_path
+    end
+  end
+
+  def index
+  end
+
+  def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:user_id, :category_id, :tea, :teacake, :tea_image_id, :comment)
   end
 end
