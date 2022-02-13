63bc5d4489a8747dfa4125dda33eeff1c2065035
ayano-w <ayasaku.t5@gmail.com>
2022-02-13 07:19:50 +0000

    fix comment views for Ajax to comments/destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   before_action :set_cafe
+  before_action :set_comments
   before_action :ensure_correct_user, only:[:destroy]
 
   def create
     @comment = current_user.comments.new(comment_params)
-    @comments = Comment.where(cafe_id: params[:cafe_id])
     if @comment.save
     else
       redirect_to cafe_path(params[:cafe_id]), notice: "100文字以内で投稿してください"
@@ -14,7 +14,6 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to cafe_path(params[:cafe_id])
   end
 
 
@@ -27,6 +26,11 @@ class CommentsController < ApplicationController
     @cafe = Cafe.find(params[:cafe_id])
   end
 
+  def set_comments
+    @comments = Comment.where(cafe_id: params[:cafe_id])
+  end
+
+
   def ensure_correct_user
     cafe = Cafe.find(params[:cafe_id])
     comment = Comment.find(params[:id])
@@ -34,6 +38,5 @@ class CommentsController < ApplicationController
       redirect_to root_path, notice: "他ユーザーの投稿を削除することはできません"
     end
   end
-
 
 end
